4ac09ea8aaa355278a2b8a6ff2a719d199357d42
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-01-01 03:00:09 +0600

    some basics of socket.io emit broadcast connect disconnect

--- server/index.js
+++ server/index.js
@@ -7,17 +7,29 @@ const port = 3000;
 const app = express();
 const server = createServer(app);
 
-const io = new Server(server);
+const io = new Server(server,
+    {
+        cors: {
+            origin: "http://localhost:5173",
+            methods: ["GET", "POST"],
+            credentials: true
+        }
+    }
+);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 io.on("connection", (socket)=> {
-    console.log("User connected");
-    console.log("Id: " + socket.id);
+    console.log("User connected", socket.id);
+    socket.emit("welcome", `Welcome to the socket server`);
+    socket.broadcast.emit("welcome", `${socket.id} is joined the server`);
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    })
 })
 
-app.listen(port, ()=> {
+server.listen(port, ()=> {
     console.log(`Socket server app listening on port ${port}`);
 });
